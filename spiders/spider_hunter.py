73671e0ff1083821960b97ccc742d8838f5d9335
svipvm <svipvm@yeah.net>
2023-03-04 17:58:19 +0800

    initialize hunter spider

--- spiders/spider_hunter.py
+++ spiders/spider_hunter.py
@@ -1,8 +1,8 @@
 import requests, json, time
 import pandas as pd
 
-base_url = 'https://www.5iai.com/api/enterprise/job/public/es?pageNumber={}'
-work_url = 'https://www.5iai.com/api/enterprise/job/public?id={}'
+base_url = 'https://www.5iai.com/api/resume/baseInfo/public/es?pageNumber={}'
+hunter_url = 'https://www.5iai.com/api/resume/baseInfo/public/{}'
 
 headers = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
@@ -13,9 +13,9 @@ headers = {
 
 frame_json = {}
 
-def get_one_work_information(data_json):
+def get_one_hunter_information(data_json):
     # print(data_json)
-    response = requests.get(work_url.format(data_json['id']), headers=headers)
+    response = requests.get(hunter_url.format(data_json['id']), headers=headers)
     data = json.loads(response.text)
 
     try:
@@ -26,30 +26,34 @@ def get_one_work_information(data_json):
 
     items_json = {
         'job_id': '\t' + str(data['id']),
-        'company_full_name': data['enterpriseName'],
-        'company_short_name': data_json['enterpriseExtInfo']['shortName'],
-        'position_name': data['positionName'],
-        'job_min_wage': data['minimumWage'],
-        'job_max_wage': data['maximumWage'],
-        'job_wage_kind': data['payMethod'],
-        'require_kind': data['willNature'],
-        'require_edu': data['educationalRequirements'],
-        'require_exp': data['exp'],
-        'num_people': data['count'],
-        'deadline': data['deadline'],
-        'company_ind': ','.join(eval(data_json['enterpriseExtInfo']['industry'])),
-        'position_keys': ','.join([item['labelName'] for item in data['keywordList']]),
-        'skill_keys': ','.join([item['labelName'] for item in data['skillsList']]),
-        'job_require': ''.join(data['jobRequiredments'].split('\n')),
-        'job_welfare': ','.join(eval(data['welfare'])),
-        # 'company_addr_code': eval(data['enterpriseAddress']['regionCode']), # 'provinceCode', 'cityCode'
-        'company_addr': data['enterpriseAddress']['detailedAddress']
+        'hunter_name': data['username'],
+        'hunter_sex': data['gender'],
+        'hunter_bthday': data['birthday'],
+        'hunter_addr': ''.join(eval(data['address'])),
+        'hunter_addr': data['exp'],
+        'hunter_sociality': data['politicalStatus'],
+        'hunter_eval': data['selfEvaluation'],
+        'exp_position': ','.join(eval(data['expectPosition'])),
+        'exp_min_wage': data['willSalaryStart'],
+        'exp_max_wage': data['willSalaryEnd'],
+        # 'exp_wage_kind': data['willNature'],
+        'exp_require_kind': data['willNature'],
+        'exp_industry': ','.join(eval(data['expectIndustry'])),
+        'exp_city': ''.join(eval(data['city'])),
+        'exp_report': data['arrivalTime'],
+        'resume_keys': ",".join([item['labelName'] for item in data['keywordList']]),
+        # 'job_exps': [list]
+        # 'work_exps': [list]
+        # 'project_exps': [list]
+        # 'competition_exps': [list]
+        # 'education_exps': [list]
+        # 'training_exps': [list]
+        # 'professional_keys': [list]
+        # 'skill_exps': [list]
+        # 'language_exps': [list]
+        # 'cert_exps': [list]
     }
-    try:
-        items_json['company_addr_code'] = eval(data['enterpriseAddress']['regionCode'])
-    except:
-        items_json['company_addr_code'] = -1
-    # print(items_json)
+    # print(items_hunter)
 
     if len(frame_json) == 0:
         for key, _ in items_json.items():
@@ -77,14 +81,16 @@ def get_all_work_information():
         for json_item in data['content']:
             print('\t{}/{} items:'.format(num_elements, total_elements))
             try:
-                if get_one_work_information(json_item):
+                if get_one_hunter_information(json_item):
                     print('\t\t[√] id ' + json_item['id'] + " finish")
                     num_elements += 1
                 else:
                     print('\t\t[×] id ' + json_item['id'] + " error")
             except:
                 print('\t\t[×] id ' + json_item['id'] + " exception")
+            break
 
+        break
         num_page += 1
         time.sleep(0.3)
         
@@ -92,7 +98,7 @@ def get_all_work_information():
 
 
 if __name__ == '__main__':
-    info_csv_path = '../datasets/recruitment-info.csv'
+    info_csv_path = '../datasets/hunter-info.csv'
 
     get_all_work_information()
     frame_data = pd.DataFrame(frame_json)

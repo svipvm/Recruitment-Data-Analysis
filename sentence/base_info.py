237c8438e81d4bec4630535a88ce7fc5ad21524b
svipvm <svipvm@yeah.net>
2023-03-16 21:46:13 +0800

    add base database

---
+++ sentence/base_info.py
@@ -0,0 +1,155 @@
+from sentence_transformers import SentenceTransformer
+import jieba, nltk, re, cpca, torch, sys, psutil, os
+import pandas as pd
+import numpy as np
+# sys.path.append('sentence')
+from sentence_util import *
+from tqdm import tqdm
+
+require_kind_json = { 0: '实习', 1: '不限', 2: '全职'} # self to fill
+require_edu_json = { 0: '不限', 1: '技工', 2: '大专', 3: '本科', 4: '硕士', 5: '博士'}
+level_json = {'COMMONLY': 1, 'GOOD': 2, 'SKILLED': 3, 'MASTER': 4}
+require_edu_re_json = {'不限': 0, '技工': 1, '大专': 2, '本科': 3, '硕士': 4, '博士': 5}
+with_encode_items = ['pos_name', 'pos_keys', 'skill_keys']
+
+model = get_model()
+job_data, hunter_data = get_both_data()
+
+base_dict = {
+    'id': (['job_id'], ['hunter_id']),
+    'pos_name': (['position_name'], ['exp_position']),
+    'job_wage': (['job_min_wage', 'job_max_wage', 'job_wage_kind'], ['exp_min_wage', 'exp_max_wage']),
+    'job_kind': (['require_kind'], ['exp_require_kind']),
+    'exp_edu': (['require_edu'], ['education_exps']),
+    'job_years': (['require_exp'], ['hunter_exp']),
+    'pos_keys': (['position_keys', 'company_ind'], ['exp_industry']),
+    'cor_addr': (['company_addr', 'company_full_name'], ['exp_city']),
+    'skill_keys': (['skill_keys'], ['skill_exps'])
+}
+job_base_dict = {} # id_key: {sentence: ..., vector: ..., }
+hunter_base_dict = {} # id_key: {sentence: ..., vector: ..., }
+
+
+def encode_base_data(obj, obj_type: int, dict_data: dict):
+    # 0: job, 1: hunter
+    assert obj_type == 0 or obj_type == 1
+    encode_result = {}
+    for key, value in base_dict.items():
+        value = value[obj_type]
+        try:
+            sentence = obj[value].values.tolist()
+            
+            if key == 'id':
+                ojb_id = str(sentence[0])
+                encode_result[ojb_id] = {}
+            elif key == 'pos_name':
+                sentence = try_to_eval(sentence[0])
+                sentence = [word.replace('实习生', '') for word in sentence]
+            elif key == 'job_wage':
+                sentence = change_wage(*sentence)
+            elif key == 'job_kind':
+                try:
+                    sentence = require_kind_json[int(sentence[0])]
+                except:
+                    sentence = require_kind_json[1]
+            elif key == 'exp_edu':
+                try:
+                    sentence = [require_edu_json[sentence[0]]]
+                except:
+                    sentence = change_edus(try_to_eval(sentence[0]))
+            elif key == 'job_years':
+                try:
+                    sentence = change_years(sentence[0])
+                except:
+                    sentence = [0]
+            elif key == 'pos_keys':
+                sentence = delete_same_elem(multi_index_to_one(sentence))
+            elif key == 'cor_addr':
+                sentence = change_addrs(sentence)
+            elif key == 'skill_keys':
+                sentence = multi_index_to_one(sentence)
+            
+            if key != 'id':
+                encode_result[ojb_id][key] = {}
+                encode_result[ojb_id][key]['sentence'] = sentence 
+                    
+        except Exception as e:
+            print(key, e)
+        
+    # print(base_object_encode(ojb_id, encode_result[ojb_id]))
+    if not check_base_info_item(obj_type, ojb_id, encode_result[ojb_id]):
+        for key, value in base_dict.items():
+            if key in with_encode_items:
+                encode_result[ojb_id][key]['vector'] = model.encode(encode_result[ojb_id][key]['sentence'])
+    else:
+        encode_result[ojb_id] = get_base_info_item(obj_type, ojb_id)
+
+    dict_data.update(encode_result)
+
+
+def calc_base_score(job_data: dict, hunter_data: dict):
+    base_score = 1.0
+    for key, job_item in job_data.items():
+        hunter_item = hunter_data[key]
+        
+        sentence1, sentence2 = job_item['sentence'], hunter_item['sentence']
+        if key in with_encode_items:
+            vector1, vector2 = job_item['vector'], hunter_item['vector']
+            score = every_multi_score(vector1, vector2)
+        elif key == 'job_wage':
+            sentence1 = np.mean(sentence1)
+            sentence2 = np.mean(sentence2)
+            sub_wage = sentence1 - sentence2
+            if sub_wage > 0: score = 1
+            else:
+                score = max(0, 1 + sub_wage / 1000)
+        elif key == 'job_kind':
+            score = float(sentence1 == sentence2)
+        elif key == 'exp_edu':
+            score = 0.2 if len(sentence2) == 0 else float(
+                        get_max_edu_level(sentence1, require_edu_re_json) <= \
+                            get_max_edu_level(sentence2, require_edu_re_json))
+        elif key == 'job_years':
+            score = float(np.min(sentence1) <= np.max(sentence2))
+        elif key == 'cor_addr':
+            if re.findall(r'(.*)省', sentence1) == re.findall(r'(.*)省', sentence2):
+                score = 0.5
+                if re.findall(r'(.*)市', sentence1) == re.findall(r'(.*)市', sentence2):
+                    score = 1
+            else:
+                score = 0.2
+        base_score *= score
+    return base_score
+
+if __name__ == '__main__':
+    # size = job_data.shape[0]
+    size = 100
+    for index_ in tqdm(range(size)):
+        job = job_data.iloc[index_,:]
+        encode_base_data(job, 0, job_base_dict)
+    save_base_info_database(0, job_base_dict)
+    # size = hunter_data.shape[0]
+    size = 100
+    for index_ in tqdm(range(size)):
+        hunter = hunter_data.iloc[index_,:]
+        encode_base_data(hunter, 1, hunter_base_dict)
+    save_base_info_database(1, hunter_base_dict)
+    
+
+    result = []
+    for key1, job_item in job_base_dict.items():
+        part_result = []
+        for key2, hunter_item in hunter_base_dict.items():
+            # print(job_item, hunter_item)
+            part_result.append(calc_base_score(job_item, hunter_item))
+        #     break
+        result.append(part_result)
+
+    for inx, (job_id, job_info) in enumerate(job_base_dict.items()):
+    # for _, (hunter_id, hunter_info) in enumerate(hunter_base_dict.items()):
+        iny = np.argmax(result, axis=1)[inx]
+        print(show_base_info(job_base_dict, inx),
+            '\n',
+            show_base_info(hunter_base_dict, iny),
+            '\nscore:',
+            result[inx][iny])
---
+++ sentence/sentence_util.py
@@ -0,0 +1,131 @@
+from sentence_transformers import SentenceTransformer, util
+import cpca, re, json, torch
+import numpy as np
+import pandas as pd
+
+job_csv_file = 'datasets/recruitment-info.csv'
+hunter_csv_file = 'datasets/hunter-info.csv'
+job_data = pd.read_csv(job_csv_file, encoding='GBK')
+hunter_data = pd.read_csv(hunter_csv_file, encoding='GBK')
+
+base_job_info_bak_file = 'database/base_job_info.json'
+try:
+    with open(base_job_info_bak_file, 'r') as f:
+        base_job_info_bak = f.read()
+        base_job_info_bak = json.loads(base_job_info_bak)
+except:
+    base_job_info_bak = {}
+
+try:
+    base_hunter_info_bak_file = 'database/base_hunter_info.json'
+    with open(base_hunter_info_bak_file, 'r') as f:
+        base_hunter_info_bak = f.read()
+        base_hunter_info_bak = json.loads(base_hunter_info_bak)
+except:
+    base_hunter_info_bak = {}
+
+model_path = '/home/vmice/projects/sbert-base-chinese-nli'
+model = SentenceTransformer(model_path)
+
+class NpEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        if isinstance(obj, np.floating):
+            return float(obj)
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
+def get_model():
+    return model
+
+def get_both_data():
+    return job_data, hunter_data
+
+def multi_index_to_one(item_list):
+    if isinstance(item_list, str):
+        return [item_list]
+    result = None
+    for item in item_list:
+        if not result:
+            result = eval(item) 
+            result += eval(item)
+    return result
+
+def try_to_eval(item):
+    try:
+        return eval(item)
+    except:
+        return [item]
+    
+def change_wage(min_wage, max_wage, wage_kind=1):
+    return [min_wage // 12, max_wage // 12] if wage_kind == 0 \
+        else ([min_wage * 30, max_wage * 30] if wage_kind == 2 else [min_wage, max_wage])
+
+def change_addrs(addrs):
+    if not isinstance(addrs, list): addrs = [addrs]
+    province, city = None, None
+    # print(addrs)
+    for addr in addrs:
+        addr = cpca.transform([addr])
+        if not province and addr['省'][0]: province = addr['省'][0] 
+        if not city and addr['市'][0]: city = addr['市'][0] 
+        # print(province, city)
+    return (str(province) + str(city)).replace('None', '').replace('市县', '市')
+
+def change_edus(edus):
+    return [re.findall(r'\[(.*)\]', edu)[0] for edu in edus]
+
+def delete_same_elem(list_like):
+    return np.unique(np.array(list_like)).tolist()
+
+def change_years(years):
+    years = re.findall(r'(\d+)', years)
+    years = [int(year) for year in years]
+    if len(years) == 0: years = [0]
+    return years
+
+def every_multi_score(vector1, vector2, method='mean'):
+    if not isinstance(vector1, torch.Tensor): vector1 = torch.tensor(vector1)
+    if not isinstance(vector2, torch.Tensor): vector2 = torch.tensor(vector2)
+    if vector1.shape[0] == 0: return 1
+    if vector2.shape[0] == 0: return 0.6
+    # print(vector1.shape, vector2.shape)
+    cos_score = util.cos_sim(vector1, vector2).numpy()
+    scores = [np.max(cos_score[i, :], keepdims=False) for i in range(cos_score.shape[0])]
+    if method == 'mean':
+        multi_score = np.mean(scores)
+    else:
+        multi_score = np.dot(scores, [1] * len(scores))
+    return multi_score
+
+def get_max_edu_level(sentence, level_json):
+    level = [level_json[edu] for edu in sentence]
+    return np.max(level, keepdims=False)
+
+def show_base_info(base_dict, index):
+    return '。'.join([str(value['sentence']) for _, value in base_dict[list(base_dict.keys())[index]].items()])
+
+def base_object_encode(info_id, info_dict: dict):
+    return '。'.join([info_id] + [str(value['sentence']) for _, value in info_dict.items()])
+
+def check_base_info_item(obj_type, info_id, info_dict):
+    item_value = base_object_encode(info_id, info_dict)
+    base_info_bak = base_job_info_bak if obj_type == 0 else base_hunter_info_bak
+    if info_id not in base_info_bak:
+        return False
+    else:
+        item_back_value = base_object_encode(info_id, base_info_bak[info_id])
+        return item_back_value == item_value
+
+def get_base_info_item(obj_type, info_id):
+    base_info_bak = base_job_info_bak if obj_type == 0 else base_hunter_info_bak
+    assert info_id in base_info_bak
+    return base_info_bak[info_id]
+
+def save_base_info_database(obj_type, data_dict):
+    base_info_bak_file = base_job_info_bak_file if obj_type == 0 else base_hunter_info_bak_file
+    # print(type(data_dict))
+    with open(base_info_bak_file, 'w') as f:
+        f.write(json.dumps(data_dict, cls=NpEncoder))
